d3b753de313b9b63b1c8a8707424c774c97df54c
Enes Aldemir <aldemirenes@live.com>
2017-06-19 14:58:49 +0300

    Upgrade Snowplow services to R85 (closes #81)

--- scripts/5_setup_init.sh
+++ scripts/5_setup_init.sh
@@ -17,21 +17,21 @@ sudo cp $init_dir/kibana4_init /etc/init.d
 sudo chmod 0755 /etc/init.d/kibana4_init
 sudo update-rc.d kibana4_init defaults
 
-sudo cp $init_dir/snowplow_stream_collector_0.6.0 /etc/init.d
-sudo chmod 0755 /etc/init.d/snowplow_stream_collector_0.6.0
-sudo update-rc.d snowplow_stream_collector_0.6.0 defaults
+sudo cp $init_dir/snowplow_stream_collector_0.9.0 /etc/init.d
+sudo chmod 0755 /etc/init.d/snowplow_stream_collector_0.9.0
+sudo update-rc.d snowplow_stream_collector_0.9.0 defaults
 
-sudo cp $init_dir/snowplow_stream_enrich_0.7.0 /etc/init.d
-sudo chmod 0755 /etc/init.d/snowplow_stream_enrich_0.7.0
-sudo update-rc.d snowplow_stream_enrich_0.7.0 defaults
+sudo cp $init_dir/snowplow_stream_enrich_0.10.0 /etc/init.d
+sudo chmod 0755 /etc/init.d/snowplow_stream_enrich_0.10.0
+sudo update-rc.d snowplow_stream_enrich_0.10.0 defaults
 
-sudo cp $init_dir/snowplow_elasticsearch_sink_good_0.5.0 /etc/init.d
-sudo chmod 0755 /etc/init.d/snowplow_elasticsearch_sink_good_0.5.0
-sudo update-rc.d snowplow_elasticsearch_sink_good_0.5.0 defaults
+sudo cp $init_dir/snowplow_elasticsearch_sink_good_0.8.0 /etc/init.d
+sudo chmod 0755 /etc/init.d/snowplow_elasticsearch_sink_good_0.8.0
+sudo update-rc.d snowplow_elasticsearch_sink_good_0.8.0 defaults
 
-sudo cp $init_dir/snowplow_elasticsearch_sink_bad_0.5.0 /etc/init.d
-sudo chmod 0755 /etc/init.d/snowplow_elasticsearch_sink_bad_0.5.0
-sudo update-rc.d snowplow_elasticsearch_sink_bad_0.5.0 defaults
+sudo cp $init_dir/snowplow_elasticsearch_sink_bad_0.8.0 /etc/init.d
+sudo chmod 0755 /etc/init.d/snowplow_elasticsearch_sink_bad_0.8.0
+sudo update-rc.d snowplow_elasticsearch_sink_bad_0.8.0 defaults
 
 sudo cp $init_dir/iglu_server_0.2.0 /etc/init.d
 sudo chmod 0755 /etc/init.d/iglu_server_0.2.0
