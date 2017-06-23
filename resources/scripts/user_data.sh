a3557dc1816349118e9e15b1020140f13e7c1efc
Enes Aldemir <aldemirenes@live.com>
2017-06-23 12:01:51 +0300

    Authenticate Iglu schema registry access from Stream Enrich (closes #92)

--- resources/scripts/user_data.sh
+++ resources/scripts/user_data.sh
@@ -4,6 +4,8 @@
 iglu_server_super_uid="980ae3ab-3aba-4ffe-a3c2-3b2e24e2ffce"
 
 # DO NOT ALTER BELOW #
+iglu_resolver_config_dir="/home/ubuntu/snowplow/configs/iglu-resolver.json"
+sed -i 's/\(.*"apikey":\)\(.*\)/\1 "'$iglu_server_super_uid'"/' $iglu_resolver_config_dir
 
 export PGPASSWORD=snowplow
 iglu_server_setup="INSERT INTO apikeys (uid, vendor_prefix, permission, createdat) VALUES ('${iglu_server_super_uid}','*','super',current_timestamp);"
