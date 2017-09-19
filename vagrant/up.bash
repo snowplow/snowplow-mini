322d970d1e3ac650b7691ec9b352f0557ee9bd85
Enes Aldemir <aldemirenes@live.com>
2017-09-19 23:50:36 +0300

    Add libffi-dev, libssl-dev, python-dev and markupsafe as dependencies (closes #133)

--- vagrant/up.bash
+++ vagrant/up.bash
@@ -4,13 +4,14 @@
 #echo "INSTALLING ANSIBLE DEPENDENCIES"
 #echo "-------------------------------"
 apt-get update
-apt-get install -y language-pack-en python-pip python-paramiko
+apt-get install -y language-pack-en python-pip python-paramiko libffi-dev libssl-dev python-dev
+sudo pip install markupsafe
+sudo pip install setuptools
 sudo sh -c 'echo "ubuntu ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers' # see https://askubuntu.com/questions/192050/how-to-run-sudo-command-with-no-password
 
 #echo "=================="
 #echo "INSTALLING ANSIBLE"
 #echo "------------------"
-sudo pip install setuptools
 sudo pip install ansible
 
 #echo "=========================================="
