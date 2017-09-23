4a5f4f8f59a3a9740ee186571a13e24b155610a4
Enes Aldemir <aldemirenes@live.com>
2017-09-23 20:43:43 +0300

    Switch to using NSQ rather than named pipes (closes #24)

--- integration/integration_test.sh
+++ integration/integration_test.sh
@@ -2,10 +2,10 @@
 
 sudo service elasticsearch start
 sudo service iglu_server_0.2.0 start
-sudo service snowplow_stream_collector_0.9.0 start
-sudo service snowplow_stream_enrich_0.10.0 start
-sudo service snowplow_elasticsearch_sink_good_0.8.0 start
-sudo service snowplow_elasticsearch_sink_bad_0.8.0 start
+sudo service snowplow_stream_collector start
+sudo service snowplow_stream_enrich start
+sudo service snowplow_elasticsearch_loader_good start
+sudo service snowplow_elasticsearch_loader_bad start
 sudo service kibana4_init start
 sudo service nginx start
 sleep 15
@@ -17,7 +17,7 @@ while [  $COUNTER -lt 10 ]; do
   curl http://localhost:8080/i
   let COUNTER=COUNTER+1
 done
-sleep 5
+sleep 60
 
 # Assertions
 good_count="$(curl --silent -XGET 'http://localhost:9200/good/good/_count' | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["count"]')"
@@ -27,7 +27,7 @@ echo "Event Counts:"
 echo " - Good: ${good_count}"
 echo " - Bad: ${bad_count}"
 
-if [[ "${good_count}" -eq "10" ]] && [[ "${bad_count}" -eq "11" ]]; then
+if [[ "${good_count}" -eq "10" ]] && [[ "${bad_count}" -eq "10" ]]; then
   exit 0
 else
   exit 1
