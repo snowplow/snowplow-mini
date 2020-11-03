a6ce7aed94c7b6020f7ebacfdfc29ddad8059d15
Oguzhan Unlu <oguzhan@snowplowanalytics.com>
2020-11-03 14:46:55 +0300

    Migrate from Travis to GitHub Actions (close #250)

--- integration/integration_test.sh
+++ integration/integration_test.sh
@@ -8,7 +8,7 @@ while [  $COUNTER -lt 10 ]; do
   curl http://localhost:8080/r/tp2
   let COUNTER=COUNTER+1
 done
-sleep 120
+sleep 30
 
 # Assertions
 good_count="$(curl --silent -XGET 'http://localhost:9200/good/good/_count' | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["count"]')"
