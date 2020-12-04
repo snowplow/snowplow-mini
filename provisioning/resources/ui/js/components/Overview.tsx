cb3d4ad379af44183899e2d9aad85bfe512c76fa
Oguzhan Unlu <oguzhan@snowplowanalytics.com>
2020-12-04 16:46:30 +0300

    Prepare for 0.12.0 release

--- provisioning/resources/ui/js/components/Overview.tsx
+++ provisioning/resources/ui/js/components/Overview.tsx
@@ -52,7 +52,7 @@ export class Overview extends React.Component<{}, {}> {
         </ul>
         <h3>The software stack installed: </h3>
         <ul>
-        <li><b>Snowplow Mini 0.11.0</b></li>
+        <li><b>Snowplow Mini 0.12.0</b></li>
           <li>Snowplow Stream Collector NSQ 2.1.0</li>
           <li>Snowplow Stream Enrich NSQ 1.4.1</li>
           <li>Snowplow Elasticsearch Loader 1.0.0</li>
