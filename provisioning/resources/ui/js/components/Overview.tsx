118389061de4ad4374dd05882802efdec8a4e688
Dilyan Damyanov <dilyand@gmail.com>
2021-03-26 14:09:37 +0000

    Bump Stream Collector to 2.2.1 (#270)

--- provisioning/resources/ui/js/components/Overview.tsx
+++ provisioning/resources/ui/js/components/Overview.tsx
@@ -53,7 +53,7 @@ export class Overview extends React.Component<{}, {}> {
         <h3>The software stack installed: </h3>
         <ul>
         <li><b>Snowplow Mini 0.12.2</b></li>
-          <li>Snowplow Stream Collector NSQ 2.1.2</li>
+          <li>Snowplow Stream Collector NSQ 2.2.1</li>
           <li>Snowplow Stream Enrich NSQ 1.4.1</li>
           <li>Snowplow Elasticsearch Loader 1.0.0</li>
           <li>Snowplow Iglu Server 0.6.1</li>
