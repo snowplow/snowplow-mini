f935e201ea8beaca2fc971376aa48da6fbeacdca
Oguzhan Unlu <oguzhan@snowplowanalytics.com>
2021-11-02 20:34:44 +0300

    Bump NSQ to 1.2.1 (close #300)

--- provisioning/resources/ui/js/components/Overview.tsx
+++ provisioning/resources/ui/js/components/Overview.tsx
@@ -58,7 +58,7 @@ export class Overview extends React.Component<{}, {}> {
           <li>Snowplow Elasticsearch Loader 1.0.0</li>
           <li>Snowplow Iglu Server 0.6.1</li>
           <li>Postgres 9.5</li>
-          <li>NSQ v1.2.0</li>
+          <li>NSQ v1.2.1</li>
           <li>Elasticsearch-OSS 6.8.20</li>
           <li>Kibana-OSS 6.8.20</li>
           <li>cAdvisor 0.36.0</li>
