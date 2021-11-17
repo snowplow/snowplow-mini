8541be931f47dd39b97d63059b5d6b7f1a9fa32d
Oguzhan Unlu <oguzhan@snowplowanalytics.com>
2021-11-17 20:11:40 +0300

    Make Elasticsearch index creation fault-tolerant (close #309)

--- provisioning/resources/init/create-es-indices.sh
+++ provisioning/resources/init/create-es-indices.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Create Elasticsearch indices
-curl -H 'Content-Type: application/json' -X PUT localhost:9200/good -d @/home/ubuntu/snowplow/elasticsearch/mapping/good-mapping.json
-curl -H 'Content-Type: application/json' -X PUT localhost:9200/bad -d @/home/ubuntu/snowplow/elasticsearch/mapping/bad-mapping.json
+until $(curl --output /dev/null --silent --fail -H 'Content-Type: application/json' -X PUT localhost:9200/good -d @/home/ubuntu/snowplow/elasticsearch/mapping/good-mapping.json); do echo "creating good index"; sleep 5; done
+until $(curl --output /dev/null --silent --fail -H 'Content-Type: application/json' -X PUT localhost:9200/bad -d @/home/ubuntu/snowplow/elasticsearch/mapping/bad-mapping.json); do echo "creating bad index"; sleep 5; done
