53c6035d29791d8540788720f39553e048cc7c05
Ian Streeter <ian@snowplowanalytics.com>
2023-03-21 22:36:35 +0000

    Improve buffer settings of ES Loaders to allow for higher throughput (close #378)

--- provisioning/resources/ui/js/components/Overview.tsx
+++ provisioning/resources/ui/js/components/Overview.tsx
@@ -55,7 +55,7 @@ export class Overview extends React.Component<{}, {}> {
         <li><b>Snowplow Mini 0.15.1</b></li>
           <li>Snowplow Stream Collector NSQ 2.8.2</li>
           <li>Snowplow Stream Enrich NSQ 3.7.0</li>
-          <li>Snowplow Elasticsearch Loader 2.0.8</li>
+          <li>Snowplow Elasticsearch Loader 2.1.0</li>
           <li>Snowplow Iglu Server 0.9.0</li>
           <li>Postgres 15.1</li>
           <li>NSQ v1.2.1</li>
