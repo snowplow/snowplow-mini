717ea8f85085b3e8aac872d5e5b1dec7661452e6
Ian Streeter <ian@snowplowanalytics.com>
2023-03-22 21:20:28 +0000

    Prepare for 0.16.0 release

--- provisioning/resources/ui/js/components/Overview.tsx
+++ provisioning/resources/ui/js/components/Overview.tsx
@@ -52,7 +52,7 @@ export class Overview extends React.Component<{}, {}> {
         </ul>
         <h3>The software stack installed: </h3>
         <ul>
-        <li><b>Snowplow Mini 0.15.1</b></li>
+        <li><b>Snowplow Mini 0.16.0</b></li>
           <li>Snowplow Stream Collector NSQ 2.8.2</li>
           <li>Snowplow Stream Enrich NSQ 3.7.0</li>
           <li>Snowplow Elasticsearch Loader 2.1.0</li>
