87572a0ca413e8a6d82c71f9a36aa1ca95206fb3
Oguzhan Unlu <oguzhan@snowplowanalytics.com>
2023-04-25 19:51:14 +0300

    Make index pattern creation error resilient (close #379)

--- provisioning/resources/init/create-kibana-indexes.sh
+++ provisioning/resources/init/create-kibana-indexes.sh
@@ -1,33 +1,10 @@
 #!/bin/sh
 
 # Create Kibana index patterns
-curl -X POST \
-  http://localhost:5601/api/saved_objects/index-pattern/good \
-  -H 'Content-Type: application/json' \
-  -H 'osd-xsrf: true' \
-  -d '{
-  "attributes": {
-    "title": "good",
-    "timeFieldName": "collector_tstamp"
-  }
-}'
 
-curl -X POST \
-  http://localhost:5601/api/saved_objects/index-pattern/bad \
-  -H 'Content-Type: application/json' \
-  -H 'osd-xsrf: true' \
-  -d '{
-  "attributes": {
-    "title": "bad",
-    "timeFieldName": "data.failure.timestamp"
-  }
-}'
+until $(curl --output /dev/null --silent --fail -H 'Content-Type: application/json' -H 'osd-xsrf: true' -X POST http://localhost:5601/api/saved_objects/index-pattern/good -d '{"attributes": {"title": "good","timeFieldName": "collector_tstamp"}}'); do echo "creating good index pattern"; sleep 5; done
+
+until $(curl --output /dev/null --silent --fail -H 'Content-Type: application/json' -H 'osd-xsrf: true' -X POST http://localhost:5601/api/saved_objects/index-pattern/bad -d '{"attributes": {"title": "bad","timeFieldName": "data.failure.timestamp"}}'); do echo "creating bad index pattern"; sleep 5; done
 
 # Set `good` as default index pattern
-curl -X POST \
-  http://localhost:5601/api/opensearch-dashboards/settings/defaultIndex \
-  -H "Content-Type: application/json" \
-  -H "osd-xsrf: true" \
-  -d '{
-  "value": "good"
-}'
+until $(curl --output /dev/null --silent --fail -H 'Content-Type: application/json' -H 'osd-xsrf: true' -X POST http://localhost:5601/api/opensearch-dashboards/settings/defaultIndex -d '{"value": "good"}'); do echo "setting [good] as the default index pattern"; sleep 5; done
