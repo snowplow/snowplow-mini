bc5403c3e34639afb3bed63253fbcb07f2acb5c2
Benjamin Benoist <benjamin.benoist@snowplowanalytics.com>
2023-05-25 13:08:02 +0200

    Bump Enrich to 3.8.0 (close #382)

--- provisioning/resources/ui/js/components/Overview.tsx
+++ provisioning/resources/ui/js/components/Overview.tsx
@@ -54,7 +54,7 @@ export class Overview extends React.Component<{}, {}> {
         <ul>
         <li><b>Snowplow Mini 0.16.0</b></li>
           <li>Snowplow Stream Collector NSQ 2.8.2</li>
-          <li>Snowplow Stream Enrich NSQ 3.7.0</li>
+          <li>Snowplow Stream Enrich NSQ 3.8.0</li>
           <li>Snowplow Elasticsearch Loader 2.1.0</li>
           <li>Snowplow Iglu Server 0.9.0</li>
           <li>Postgres 15.1</li>
