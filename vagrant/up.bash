73903c6fa22679b543fb41888d5fb8b1cc2254f1
spenes <enes@snowplowanalytics.com>
2023-09-26 14:24:17 +0300

    Use pip2 (close #389)

--- vagrant/up.bash
+++ vagrant/up.bash
@@ -5,17 +5,17 @@
 #echo "-------------------------------"
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -y language-pack-en python-pip libffi-dev libssl-dev python-dev
-sudo pip install --upgrade pip
-sudo pip install markupsafe==1.1.1
-sudo pip install setuptools==40.8.0
-sudo pip install paramiko==1.16.0
+apt-get install -y language-pack-en python-pip libffi-dev libssl-dev python2-dev
+sudo pip2 install --upgrade pip
+sudo pip2 install markupsafe==1.1.1
+sudo pip2 install setuptools==40.8.0
+sudo pip2 install paramiko==1.16.0
 sudo sh -c 'echo "ubuntu ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers' # see https://askubuntu.com/questions/192050/how-to-run-sudo-command-with-no-password
 
 #echo "=================="
 #echo "INSTALLING ANSIBLE"
 #echo "------------------"
-sudo pip install ansible==2.8.1
+sudo pip2 install ansible==2.8.1
 
 #echo "=========================================="
 #echo "RUNNING PLAYBOOKS WITH ANSIBLE*"
