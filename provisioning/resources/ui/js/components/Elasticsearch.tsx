1b33a0fe17ffe24d983e634dac1ab60292691022
spenes <enes@snowplowanalytics.com>
2023-09-28 13:46:21 +0300

    Add Snowplow Community License

--- provisioning/resources/ui/js/components/Elasticsearch.tsx
+++ provisioning/resources/ui/js/components/Elasticsearch.tsx
@@ -1,15 +1,11 @@
 /*
- * Copyright (c) 2016 Snowplow Analytics Ltd. All rights reserved.
+ * Copyright (c) 2016-present Snowplow Analytics Ltd. All rights reserved.
  *
- * This program is licensed to you under the Apache License Version 2.0,
- * and you may not use this file except in compliance with the Apache License Version 2.0.
- * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the Apache License Version 2.0 is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ * This program is licensed to you under the Snowplow Community License Version 1.0,
+ * and you may not use this file except in compliance with the Snowplow Community License Version 1.0.
+ * You may obtain a copy of the Snowplow Community License Version 1.0 at https://docs.snowplow.io/community-license-1.0
  */
+
 
 /// <reference path="../../typings/node/node.d.ts" />
 /// <reference path="../../typings/react/react.d.ts" />
