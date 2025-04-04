55060330a3fb023741216dc62ecc041851e6369e
Oguzhan Unlu <oguzhan@snowplowanalytics.com>
2025-04-04 10:45:52 +0300

    Migrate vm setup to Python 3

--- vagrant/up.bash
+++ vagrant/up.bash
@@ -5,17 +5,15 @@
 #echo "-------------------------------"
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -y language-pack-en python-pip libffi-dev libssl-dev python2-dev
-sudo pip2 install --upgrade pip
-sudo pip2 install markupsafe==1.1.1
-sudo pip2 install setuptools==40.8.0
-sudo pip2 install paramiko==1.16.0
+apt-get install -y language-pack-en libffi-dev libssl-dev python3-dev python3-pip
+sudo pip install --upgrade pip
+sudo pip install --upgrade cryptography
 sudo sh -c 'echo "ubuntu ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers' # see https://askubuntu.com/questions/192050/how-to-run-sudo-command-with-no-password
 
 #echo "=================="
 #echo "INSTALLING ANSIBLE"
 #echo "------------------"
-sudo pip2 install ansible==2.8.1
+sudo pip install ansible==2.8.1
 
 #echo "=========================================="
 #echo "RUNNING PLAYBOOKS WITH ANSIBLE*"
